5bd4dd80085750f2fe0d7edb4d5df44f9018415e
yonatan <yonatanhuamanc@gmail.com>
2020-08-30 23:40:22 -0300

    act

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: "./src/app/index.js",
+  output: {
+    path: __dirname + "/src/public",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css?$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+        ],
+      },
+    ],
+  },
+};
